81d147404bb93fd9669305882c67c91e531ae157
Debargha Mondal <166617838+DebarghaMondal@users.noreply.github.com>
2024-08-06 10:48:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-//your JS code here. If required.
-const levelItem = document.getElementById('level');
+// //your JS code here. If required.
+// const levelItem = document.getElementById('level');
 
-const parentUl = levelItem.parentNode;
+// const parentUl = levelItem.parentNode;
 
-const listItems = parentUl.getElementByTagName('li');
+// const listItems = parentUl.getElementByTagName('li');
 
-const index = Array.prototype.indexOf.call(listItems, levelItem);
+// const index = Array.prototype.indexOf.call(listItems, levelItem);
 
-alert("The level of the element is:" index);
+// alert("The level of the element is:" index);
+
+let level = 0;
+let currentElement = document.getElementById('level');
+while (currentElement) {
+    level++;
+    currentElement = currentElement.parentNode;
+}
+alert("The level of the element is: " + level);
