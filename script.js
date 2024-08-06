4a10ade3f556190b28d24cb36dbd3b9d8db215bf
Debargha Mondal <166617838+DebarghaMondal@users.noreply.github.com>
2024-08-06 10:44:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const levelItem = document.getElementById('level');
+
+const parentUl = levelItem.parentNode;
+
+const listItems = parentUl.getElementByTagName('li');
+
+const index = Array.prototype.indexOf.call(listItems, levelItem);
+
+alert("The level of the element is:" index);
